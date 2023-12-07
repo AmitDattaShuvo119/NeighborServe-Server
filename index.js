6b63abeadb3ba71c2a054113332f6073bed3ddb0
salam_rahaman <salam.cse191032@gmail.com>
2023-12-07 22:38:13 +0600

    New Route added and socket added

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const bodyParser = require("body-parser");
 require("dotenv").config();
 const users = require("./routes/users");
 const providers = require("./routes/providers");
-
+const chatApp = require("./routes/chatApp");
 const port = process.env.PORT || 5000;
 
 app.use(cors());
@@ -15,7 +15,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use("/users", users);
 app.use("/providers", providers);
-
+app.use("/chatApp", chatApp);
 app.get("/", (req, res) => {
   res.send("hello");
 });
